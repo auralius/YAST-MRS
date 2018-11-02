17cf469427e92cf8ee9ac2d47bbeb71535fce7bc
Auralius Manurung <manurunga@yandex.com>
2018-11-02 19:50:52 +0700

    added properties window

--- src/properties_window.cpp
+++ src/properties_window.cpp
@@ -6,8 +6,10 @@ CPropertiesWindow::CPropertiesWindow(int w, int h, const char *l)
     :Fl_Window(0, 0, w, h, l)
 {
     m_robot == NULL;
-    m_disp = new Fl_Text_Display(20, 20, 640 - 40, 480 - 40, "Robot properties");
-    m_tbuff = new Fl_Text_Buffer();      // text buffer
+    m_disp = new Fl_Text_Editor(20, 20, 640 - 40, 480 - 40, l);
+    m_tbuff = new Fl_Text_Buffer(0, 4096); 
+    m_disp->textfont(FL_COURIER);
+    m_disp->textsize(12);
     m_disp->buffer(m_tbuff);
 }
 
@@ -22,13 +24,14 @@ void CPropertiesWindow::set_robot_addr(CRobot *r)
     m_robot = r;
 }
 
-void CPropertiesWindow::set_disp(const char *t)
+void CPropertiesWindow::set_text(const char *t)
 {
     m_tbuff->text(t);
 }
 
 void CPropertiesWindow::clean_memory()
 {
-
+    m_tbuff->text("");
+    delete[] m_tbuff;
     delete [] m_disp;
 }
--- src/properties_window.h
+++ src/properties_window.h
@@ -2,7 +2,8 @@
 
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
-#include <FL/Fl_Text_Display.H>
+#include <FL/Fl_Text_Editor.H>
+
 
 class CRobot; // Forward declaration
 
@@ -12,11 +13,11 @@ public:
     CPropertiesWindow(int w, int h, const char *l);
     ~CPropertiesWindow(void);
     void set_robot_addr(CRobot *r);
-    void set_disp(const char *t);
+    void set_text(const char *t);
     void clean_memory();
 
 private:
-    Fl_Text_Display *m_disp;
+    Fl_Text_Editor *m_disp;
     Fl_Text_Buffer *m_tbuff;
 
     CRobot *m_robot;
